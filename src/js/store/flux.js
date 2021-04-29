a588d1c66b8e6ec960072a9be3f83d04c950b650
aserdnad <andreasgamardo@gmail.com>
2021-04-30 01:40:02 +0200

    trae los contactos y si no existe lo crea

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -4,6 +4,45 @@ const getState = ({ getStore, setStore }) => {
 			//Your data structures, A.K.A Entities
 		},
 		actions: {
+			loadData: () => {
+				const conseguirAgenda = async () => {
+					try {
+						const respuesta = await fetch("https://assets.breatheco.de/apis/fake/contact/agenda/aserdnad");
+						const data = await respuesta.json();
+						if (data.length < 1) {
+							const crearAgenda = async () => {
+								try {
+									const respuestaNoExiste = await fetch(
+										"https://assets.breatheco.de/apis/fake/contact/",
+										{
+											method: "POST",
+											headers: { "Content-Type": "application/json" },
+											body: JSON.stringify({
+												full_name: "aserdnad",
+												email: "aserdnad@gmail.com",
+												agenda_slug: "aserdnad",
+												address: "47568 NW 34ST, 33434 FL, USA",
+												phone: "0416472993"
+											})
+										}
+									);
+									const dataNoExiste = await respuestaNoExiste.json();
+									setStore({ agenda: dataNoExiste });
+								} catch (error) {
+									console.log(error);
+								}
+							};
+							crearAgenda();
+						} else {
+							setStore({ agenda: data });
+						}
+					} catch (error) {
+						console.log(error);
+					}
+				};
+				conseguirAgenda();
+			}
+
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
 		}
